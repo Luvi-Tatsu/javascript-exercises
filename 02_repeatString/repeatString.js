65bcd6935cf71a78dd8f12401191226b817cfd68
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-10 16:43:58 +0800

    Passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
 
+const number = Math.floor(Math.random() * 1000);
+const repeatString = function(string, num) {
+let reapetedString = "";
+if (num === -1)
+    return "ERROR";
+ while (num > 0){
+  reapetedString += string;
+  num--;
+ }
+ return reapetedString;
 };
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
