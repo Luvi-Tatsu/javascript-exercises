f57922638b3ea5a6c0c4522ef4f1c536a4c20645
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-12 16:48:10 +0800

    Use Math.min and max to sort values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+let min = Math.min(start,end);
+let max = Math.max (start,end);
+let sum = 0;
+if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+if (start < 0 || end < 0) return "ERROR";
+for (i = min; i <= max; i++){
+    sum += i;
+}
+return sum;
 };
 
 // Do not edit below this line
