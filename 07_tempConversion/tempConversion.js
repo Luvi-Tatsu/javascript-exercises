1609262e7399ad69b60f7809dfebfa52a39ecc87
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-13 13:02:18 +0800

    change params to make sense, I think my code is much more readable (solution is simplified);

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,14 @@
 
-const convertToCelsius = function(valCelsius) {
+const convertToCelsius = function(fahrenheit) {
   let ratioSize = 5/9;
-  let celsius = (valCelsius - 32) * ratioSize;
+  let celsius = (fahrenheit - 32) * ratioSize;
   return Math.round(celsius *10) /10;    
 
 };
 
-const convertToFahrenheit = function(valFahrenheit) {
+const convertToFahrenheit = function(celsius) {
   let ratioSize = 9/5;
-  let fahrenheit = 32+ valFahrenheit * ratioSize ;
+  let fahrenheit = 32+ celsius * ratioSize ;
   return Math.round(fahrenheit *10) /10;
 };
 
