d91c0604b24ecf37b4b7f9fd764037c2c2040d0a
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-13 11:02:42 +0800

    &&,|| logical operators

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(leapYear) {
 
+if (leapYear % 4 != 0 || leapYear % 400 !=0 && leapYear % 100 === 0){
+    return false;
+}
+else {
+    return true;
+}
 };
 
 // Do not edit below this line
