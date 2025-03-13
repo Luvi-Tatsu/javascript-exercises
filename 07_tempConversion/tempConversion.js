ee168dd59411b18f573ea3308eeb9ef57da76f53
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-03-13 12:59:51 +0800

    Used Math Round for Decimals and RatioSize to define fraction

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+
+const convertToCelsius = function(valCelsius) {
+  let ratioSize = 5/9;
+  let celsius = (valCelsius - 32) * ratioSize;
+  return Math.round(celsius *10) /10;    
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(valFahrenheit) {
+  let ratioSize = 9/5;
+  let fahrenheit = 32+ valFahrenheit * ratioSize ;
+  return Math.round(fahrenheit *10) /10;
 };
 
 // Do not edit below this line
