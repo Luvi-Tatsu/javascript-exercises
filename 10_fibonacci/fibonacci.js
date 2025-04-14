ac6246dc7b9293d1a63ec630151e5b40c689b9f8
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-04-14 20:53:51 +0800

    Fibonacci Assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const num = parseInt(n);
+    let n1 = 0, n2 = 1, fn;
+    for (let i = 1; i <= num; i++){
+        fn = n1 + n2;
+        n1 = n2;
+        n2 = fn;
+    }
+    if (num < 0){
+        return "OOPS"
+    }
+    return n1;
+    
 };
 
 // Do not edit below this line
