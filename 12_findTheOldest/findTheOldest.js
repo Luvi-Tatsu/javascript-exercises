cdfe53233a0d6891975ef60a7c264766d275c618
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-04-15 10:06:28 +0800

    learned how to check the date today using javascript

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+ return arr.reduce((oldest, offPerson)=>{
+    const oldestPer = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const age = getAge(offPerson.yearOfBirth, offPerson.yearOfDeath);
+    return oldestPer < age ? offPerson : oldest;
+});
 };
+
+const getAge = function(birth,death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
